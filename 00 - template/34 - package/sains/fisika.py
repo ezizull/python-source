d0f51a14908ac4908c27c2318874aa0025a4516e
ezizull <71552760+ezizull@users.noreply.github.com>
2021-02-21 20:21:46 +0700

    Delete 00 - template directory
    Delete 00 - template directory

--- 00 - template/34 - package/sains/fisika.py
+++
@@ -1,8 +0,0 @@
-def kecepatan(jarak, waktu):
-    print('meghitung kecepatan')
-    return jarak / waktu
- 
-def waktu_tempuh(kecepatan, jarak):
-    print('meghitung waktu temput')
-    return jarak / kecepatan
-
--- 00 - template/35 - main/main.py
+++
@@ -1,6 +0,0 @@
-import matematika as mm
-
-mm.tambah(3,5)
-mm.kurang(5,3)
-
-# print(__name__)
